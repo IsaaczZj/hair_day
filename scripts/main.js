6224a8343fb85a044f4bda1a4e80b7326639a129
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-04-08 11:41:19 -0300

    configuracao finalizada

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+import "../libs/dayJs.js"
+
+import "../styles/globals.css"
+import "../styles/form.css"
+import "../styles/schedule.css"
+
+import dayjs from "dayjs"
+console.log(dayjs().format("HH:mm"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebPackPlugin = require("copy-webpack-plugin");
+
 
 module.exports = {
   target: "web",
@@ -6,6 +9,51 @@ module.exports = {
   entry: path.resolve(__dirname, "scripts", "main.js"),
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  }
-}
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve(__dirname, "assets/scissors.svg")
+    }),
+    new CopyWebPackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "assets"),
+          to: path.resolve(__dirname, "dist", "assets")
+        }
+      ]
+    })
+    
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude:  /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+
+};
