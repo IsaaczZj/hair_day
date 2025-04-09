09a20db228a703f5d22e8a51d2aad491ad588c9d
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-04-09 10:32:03 -0300

    criando logica de horarios disponiveis baseado nos horarios

--- modules/schedules/load.js
+++ modules/schedules/load.js
@@ -1,3 +1,16 @@
+import { hoursLoad } from "../form/hours-load.js";
+
+
+const selectedDate = document.getElementById("date")
+
+
 export function schedulesDay(){
-  
+  //Obtem a data do input
+  const date = selectedDate.value
+  hoursLoad({date})
+
+
+  //Buscar na api os agendamentos para carregar no lado direto
+
+  //Os horarios disponiveis (horarios no futuro + nao agendados) do lado esquerdo
 }
